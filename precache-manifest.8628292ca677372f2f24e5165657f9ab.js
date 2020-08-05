6add68991cc83a887c08133db365c60e1ca99edd
Edward Tan <guofoo@gmail.com>
2020-08-05 15:49:25 -0700

    Updates

---
+++ precache-manifest.8628292ca677372f2f24e5165657f9ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "584cd6c06737a9550a5d9d30ba7aea2d",
+    "url": "/ieee-web/index.html"
+  },
+  {
+    "revision": "a16c46538bffad791888",
+    "url": "/ieee-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8cfe70dacc25a021820",
+    "url": "/ieee-web/static/js/2.3063f592.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ieee-web/static/js/2.3063f592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16c46538bffad791888",
+    "url": "/ieee-web/static/js/main.9a4a45d7.chunk.js"
+  },
+  {
+    "revision": "6331c60b6deb3e02ebf4",
+    "url": "/ieee-web/static/js/runtime-main.d366f01a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ieee-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
